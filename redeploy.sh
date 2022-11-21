ded11f24c852e124a4a254b2f50c47fb92089bd6
-Denux <timon.klinkert@gmail.com>
2022-11-21 13:48:27 +0100

    updated `redeploy.sh`

--- redeploy.sh
+++ redeploy.sh
@@ -7,7 +7,7 @@ rm -rf $DIR/redeploy/
 git clone https://github.com/DenuxPlays/Denux-Dashboard.git redeploy
 cd redeploy
 
-bash trunk build --release
+trunk build --release
 
 if [ $? -eq 0 ]; then
         rm -r $DIR/deploy
