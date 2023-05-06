970147370f972a0396a3372a144a3db2574b98f4
Timon Klinkert <timon.klinkert@denux.dev>
2023-05-06 02:24:10 +0200

    Leptos rewrite start

--- src/main.rs
+++ src/main.rs
@@ -1,67 +1,11 @@
-use yew::prelude::*;
-use yew_router::prelude::*;
-use bounce::helmet::{Helmet, HelmetBridge};
-use bounce::BounceRoot;
-
-#[derive(Clone, Routable, PartialEq, Eq)]
-pub enum Route {
-    #[at("/")]
-    Home,
-
-    #[not_found]
-    #[at("/404")]
-    NotFound,
-}
-
-fn switch(routes: Route) -> Html {
-    return match routes {
-        Route::Home => html! { <Home /> },
-        Route::NotFound => html! { <NotFound /> },
-    };
-}
-
-#[function_component(App)]
-fn app() -> Html {
-    return html! {
-        <BounceRoot>
-            <HelmetBridge default_title="Denux"/>
-            <BrowserRouter>
-                <Switch<Route> render={switch} />
-            </BrowserRouter>
-        </BounceRoot>
-    };
-}
-
-#[function_component(Home)]
-fn home() -> Html {
-    return html! {
-        <>
-        <Helmet>
-            <link rel="stylesheet" href="assets/css/main.css" />
-            <link rel="icon" href="assets/img/ServerLogo.jpg" />
-        </Helmet>
-            <body>
-                <p>{"Nothing to see"}</p>
-            </body>
-        </>
-    };
-}
-
-#[function_component(NotFound)]
-pub fn not_found() -> Html {
-    return html! {
-        <>
-            <Helmet>
-                <title>{"Page not found"}</title>
-                <link rel="stylesheet" href="assets/css/sites/404.css" />
-                <link rel="icon" href="assets/img/ServerLogo.jpg" />
-            </Helmet>
-            <p>{"URL not found"}</p>
-            <button><Link<Route> to={Route::Home}>{ "click here to go home" }</Link<Route>></button>
-        </>
-    };
-}
+use leptos::*;
 
 fn main() {
-    yew::Renderer::<App>::new().render();
+    mount_to_body(|cx| view! {
+        cx,
+        <div>
+            <h1>" Hello, world!"</h1>
+            <p>" This is a paragraph. "</p>
+        </div>
+    })
 }
